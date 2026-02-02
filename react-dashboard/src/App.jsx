6d3dee9c4a2f8872db0f088ea975f2b8704b1105
shiftdrewmarlo <drewmarlo1234@gmail.com>
2026-02-02 22:15:53 +0800

    Add dashboard files

---
+++ react-dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+
+export default function App() {
+  return (
+    <div className="app-root">
+      <Sidebar />
+      <main className="main-content">
+        <Dashboard />
+      </main>
+    </div>
+  );
+}
